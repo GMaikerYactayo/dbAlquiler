afe0d3b65d0c72a62f4fc34fb11e0f265160d384
MaikerGonzales <maiker.gonzales@vallegrande.edu.pe>
2018-11-26 09:23:11 -0500

    He creado la base de datos Alquiler

--- scriptBD_Alquiler.sql
+++ scriptBD_Alquiler.sql
@@ -3,3 +3,12 @@ show DATABASEs;
 
 /*Eliminar bd alquiler*/
 DROP DATABASE dbalquiler;
+
+/*Crear nustra bd alquiler teniendo en cuenta la configuracion de caracteres*/
+CREATE DATABASE dbalquiler DEFAULT CHARACTER set utf8;
+
+/*Poner en uso la base de datos*/
+USE dbalquiler;
+
+/*Verificar que la bd este en usu*/
+SELECT DATABASE();
